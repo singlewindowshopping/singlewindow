051b638a8fb185f8fa051978cf0d2a61ecf114ae
singlewindowshopping <singlewindowshopping>
2019-09-07 22:26:24 -0400

    services

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  //database: "mongodb+srv://singlewindow:shopping@cluster0-txjgm.mongodb.net/test",   //prod
+  mongoURI: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: "yoursecret"
+}
